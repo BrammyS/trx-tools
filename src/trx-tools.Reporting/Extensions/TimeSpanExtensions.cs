3777a2d2f84f9c29eb994adb6edd9563eef98cdd
Bram <bram.esendam@gmail.com>
2025-02-12 20:43:05 +0100

    Code cleanup

--- src/trx-tools.Reporting/Extensions/TimeSpanExtensions.cs
+++ src/trx-tools.Reporting/Extensions/TimeSpanExtensions.cs
@@ -6,30 +6,22 @@ public static class TimeSpanExtensions
     {
         var values = new List<string>();
         if (timeSpan.Days > 0)
-        {
             values.Add($"{timeSpan.Days}d");
-        }
 
         if (timeSpan.Hours > 0)
-        {
             values.Add($"{timeSpan.Hours}h");
-        }
 
         if (timeSpan.Minutes > 0)
-        {
             values.Add($"{timeSpan.Minutes}m");
-        }
 
         if (timeSpan.Seconds > 0)
-        {
             values.Add($"{timeSpan.Seconds}s");
-        }
 
         if (timeSpan.Milliseconds > 0)
             values.Add($"{timeSpan.Milliseconds}ms");
         else if (values.Count == 0)
             values.Add("< 1ms");
-
+        
         return string.Join(" ", values);
     }
 }
